c5180bf6ddc8f45b1a17d0c362458edff26d5128
m.aurora <naruto06@mail.ru>
2023-03-27 13:06:30 +0100

    deploy functions

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
-const admin = require('firebase-admin');
 const axios = require('axios');
+const { OAuth2Client } = require('google-auth-library');
+const admin = require('firebase-admin');
 
 admin.initializeApp();
 
@@ -92,3 +93,103 @@ exports.getPostById = functions.https.onCall((data, context) => {
             throw new functions.https.HttpsError('internal', error.message);
         });
 });
+
+exports.authenticateUser = functions.https.onCall((data, context) => {
+    const { email, password } = data;
+
+    return admin.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // User authenticated successfully
+            const user = userCredential.user;
+            // Return user data
+            return { success: true, uid: user.uid, email: user.email };
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            return { error: errorMessage };
+        });
+});
+
+exports.registerUser = functions.https.onCall(async (data, context) => {
+    const { email, password, name } = data;
+
+    try {
+        const userRecord = await admin.auth().createUser({
+            email,
+            password,
+            displayName: name
+        });
+
+        await admin.auth().sendEmailVerification(userRecord.uid);
+
+        return { success: true, message: 'User registered successfully. Please check your email to verify your account.' };
+    } catch (error) {
+        console.error(error);
+        return { success: false, message: 'Registration failed.' };
+    }
+});
+
+exports.verifyEmail = functions.https.onRequest(async (req, res) => {
+    const { mode, oobCode } = req.query;
+
+    try {
+        if (mode !== 'verifyEmail') {
+            throw new Error('Invalid verification request.');
+        }
+
+        await admin.auth().applyActionCode(oobCode);
+
+        res.send('Your email has been verified!');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Email verification failed.');
+    }
+});
+
+exports.signInWithFacebook = functions.https.onCall((data, context) => {
+    const { accessToken } = data;
+    const credential = admin.auth.FacebookAuthProvider.credential(accessToken);
+    return admin.auth().signInWithCredential(credential)
+        .then((result) => {
+            // Handle successful authentication
+            const user = result.user;
+            console.log(user);
+            return { message: 'Successfully signed in with Facebook!' };
+        })
+        .catch((error) => {
+            // Handle authentication errors
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+            return { error: errorMessage };
+        });
+});
+
+exports.createPost = functions.https.onCall(async (data, context) => {
+
+    if (!context.auth) {
+        throw new functions.https.HttpsError('unauthenticated', 'You must be logged in to create a post.');
+    }
+
+    // data from client
+    const { title, description, price, categoryId, location, image } = data;
+
+    const userId = context.auth.uid;
+
+    const newPost = {
+        title,
+        description,
+        price,
+        categoryId,
+        location,
+        image,
+        userId,
+        createdAt: admin.firestore.FieldValue.serverTimestamp()
+    };
+
+    const newPostRef = await admin.firestore().collection('posts').add(newPost);
+
+    return { id: newPostRef.id };
+});
+
