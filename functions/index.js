84fef6dc89e2ecc6d4fa2d998d9d16caea49b199
m.aurora <naruto06@mail.ru>
2023-06-15 14:17:48 +0100

    Add delete images logic

--- functions/index.js
+++ functions/index.js
@@ -142,11 +142,10 @@ exports.editPost = functions.https.onCall(async (data, context) => {
         throw new functions.https.HttpsError('unauthenticated', 'You must be logged in to edit a post.');
     }
 
-    const { postId, title, description, price, categoryId, location, images } = data;
+    const { postId, title, description, price, categoryId, location, images, removedImages } = data;
     const userId = context.auth.uid;
 
-    const postRef = admin.firestore().collection('posts')
-        .doc(postId);
+    const postRef = admin.firestore().collection('posts').doc(postId);
     const postDoc = await postRef.get();
 
     if (!postDoc.exists) {
@@ -159,37 +158,47 @@ exports.editPost = functions.https.onCall(async (data, context) => {
         throw new functions.https.HttpsError('permission-denied', 'You do not have permission to edit this post');
     }
 
-    // upd location
+    // Update location
     let locationRef = post.locationRef;
 
     if (location) {
-        const locationDoc = await admin.firestore().collection('locations')
-            .doc(post.locationRef.id)
-            .get();
-
+        const locationDoc = await admin.firestore().collection('locations').doc(post.locationRef.id).get();
         await locationDoc.ref.update(location);
-
         locationRef = locationDoc.ref;
     }
 
-    // upd images
-    let imageUrls = post.images;
-
-    if (images) {
-        imageUrls = await Promise.all(
-            images.map(async (imageData, i) => {
-                const fileName = `post_${postId}_${i}`;
-                const file = admin.storage().bucket()
-                    .file(fileName);
-
-                await file.save(imageData.buffer, { metadata: { contentType: imageData.mimeType } });
-
-                return file.publicUrl();
-            }),
-        );
+    // Remove images
+    if (removedImages && removedImages.length > 0) {
+        const storagePromises = removedImages.map(async imageName => {
+            const filePath = `User_${userId}/Post_${postId}/${imageName}`;
+            const file = admin.storage().bucket().file(filePath);
+            await file.delete();
+        });
+        await Promise.all(storagePromises);
     }
 
-    // upd post
+    // Add new images
+    let imageUrls = post.images || [];
+
+    if (images && images.length > 0) {
+        const uploadPromises = images.map(async ({ base64, mimeType }) => {
+            const fileName = `post_${postId}_${Date.now()}.${mimeType.split('/')[1]}`;
+            const base64WithoutPrefix = base64.replace(/^data:image\/[^;]+;base64,/, '');
+            const imageBuffer = Buffer.from(base64WithoutPrefix, 'base64');
+
+            const filePath = `User_${userId}/Post_${postId}/${fileName}`;
+            const file = admin.storage().bucket().file(filePath);
+
+            await file.save(imageBuffer, { metadata: { contentType: mimeType } });
+
+            return `https://storage.googleapis.com/${bucket.name}/${file.name}`;
+        });
+
+        const uploadedImages = await Promise.all(uploadPromises);
+        imageUrls = [...imageUrls, ...uploadedImages];
+    }
+
+    // Update post
     const postData = {
         title: title || post.title,
         description: description || post.description,
