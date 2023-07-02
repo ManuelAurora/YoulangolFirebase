3eb0d0a5ce8d36b9c165c5b0ca84eb9f0a209b5d
m.aurora <naruto06@mail.ru>
2023-07-02 17:17:55 +0100

    add rating

--- functions/index.js
+++ functions/index.js
@@ -763,6 +763,31 @@ async function filterPostsByRadius(posts, location, radius) {
     return filteredPosts.filter(post => post);
 }
 
+exports.addRating = functions.https.onCall(async (data, context) => {
+
+    if (!context.auth) {
+        throw new functions.https.HttpsError('unauthenticated', 'You must be logged in to add a rating.');
+    }
+
+    const { productName, rating, sellerId, message } = data;
+
+    const sellerRef = admin.firestore().collection('users').doc(sellerId);
+
+    const ratingRef = sellerRef.collection('rating').doc();
+    const newRating = { productName, rating, createdAt: admin.firestore.Timestamp.now().toMillis(), message };
+    await ratingRef.set(newRating);
+
+    const ratingsSnapshot = await sellerRef.collection('rating').get();
+    const totalRatings = ratingsSnapshot.size;
+    const sum = ratingsSnapshot.docs.reduce((total, doc) => total + doc.data().rating, 0);
+    const averageRating = Math.round((sum / totalRatings) * 100) / 100;
+
+    await sellerRef.update({ rating: averageRating });
+
+    return { success: true };
+
+});
+
 /**
  * Преобразование градусы в радианы.
  *
