a941c22f9ec033786d08ef96ccffa47933e64c6d
Evgeny Korotaev <korotaev.e.s@gmail.com>
2023-10-03 18:37:50 +0700

    fix sendMessage

--- functions/chat/sendMessage.js
+++ functions/chat/sendMessage.js
@@ -7,43 +7,35 @@ exports.sendMessage = functions.https.onCall(async (data, context) => {
             throw new functions.https.HttpsError('unauthenticated', 'You must be logged in to send a message.');
         }
 
-        const { text, chatId } = data;
         const senderId = context.auth.uid;
-        const timestamp = Date.now();
+
+        const { text, chatId } = data;
+
+        if (!chatId) {
+            throw new functions.https.HttpsError('invalid-argument', 'Chat ID is required.');
+        }
 
         if (!text || typeof text !== 'string') {
             throw new functions.https.HttpsError('invalid-argument', 'Text message is required and must be a string.');
         }
 
         const chatRef = admin.firestore().collection('chats').doc(chatId);
+
         const messageRef = chatRef.collection('messages').doc();
 
-        const messageData = {
-            id: messageRef.id,
-            senderId,
-            text,
-            timestamp,
-            isRead: false
-        };
+        const timestamp = Date.now();
 
         await Promise.all([
-            messageRef.set(messageData),
-            chatRef.update({
-                messages: admin.firestore.FieldValue.arrayUnion(messageData),
-                updatedAt: timestamp,
+            chatRef.update({ updatedAt: timestamp }),
+
+            messageRef.set({
+                id: messageRef.id,
+                senderId,
+                text,
+                timestamp,
+                isRead: false
             }),
         ]);
-
-        // notification
-        // const payload = {
-        //     notification: {
-        //         title: 'New message',
-        //         body: `You have a new message from ${senderId}`,
-        //         icon: '/assets/icons/icon-96x96.png',
-        //         clickAction: `https://${process.env.GCLOUD_PROJECT}.firebaseapp.com/chat/${chatId}`
-        //     }
-        // };
-        // await admin.messaging().sendToTopic(chatRef.path, payload);
 
         return { success: true };
     } catch (error) {
