a147f4fa9a544b6f153e087369fe5a46d3751356
Evgeny Korotaev <korotaev.e.s@gmail.com>
2023-10-31 09:47:39 +0700

    Added filtering for user posts by status

--- functions/posts/getPostsByUser.js
+++ functions/posts/getPostsByUser.js
@@ -1,17 +1,24 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const { POST_STATUSES } = require('../constants.js');
 
 exports.getPostsByUser = functions.https.onCall(async (data) => {
     try {
-        const { userId } = data;
+        const { userId, status } = data;
 
         if (!userId) {
             throw new functions.https.HttpsError('invalid-argument', 'User ID is required.');
         }
 
-        const query = admin.firestore().collection('posts')
+        let query = admin.firestore().collection('posts')
             .where('userId', '==', userId)
             .orderBy('createdAt', 'desc');
+
+        const isValidStatus = Object.values(POST_STATUSES).includes(status);
+
+        if (isValidStatus) {
+            query = query.where('status', '==', status)
+        }
 
         const snapshot = await query.get();
 
